0703e9ff9eb8b7b8cc6893f0bff7dad455f34d15
Rachael <srbennett@gmail.com>
2016-10-15 11:15:13 -0700

    attempt at kiss, locks up.  OOs

--- lamp-draw.lua
+++ lamp-draw.lua
@@ -19,6 +19,15 @@ return {
     fb:set(18,c) fb:set(19,c) fb:set(22,c) fb:set(23,c)
     fb:set(25,c) fb:set(28,c) fb:set(29,c) fb:set(32,c)
   end,
+  ["oo"] = function(t,fb,g,r,b)
+    t:unregister()
+    fb:fill(0,0,0)
+    local c = string.char(g,r,b)
+    fb:set( 2,c) fb:set( 3,c) fb:set( 6,c) fb:set( 7,c)
+    fb:set( 9,c) fb:set(12,c) fb:set(13,c) fb:set(16,c)
+    fb:set(17,c) fb:set(20,c) fb:set(21,c) fb:set(24,c)
+    fb:set(26,c) fb:set(27,c) fb:ser(30,c) fb:set(31,c)
+  end,
   ["laserchase"] = function(t,fb,g,r,b)
     local ix = 1
     local c = string.char(g,r,b)
@@ -26,6 +35,23 @@ return {
     t:register(50,tmr.ALARM_AUTO,function()
       fb:fade(2); fb:set(ix,c); dodraw()
       ix = ix + 1; if ix > fb:size() then ix = 1 end
+    end)
+  end,
+  ["kiss"] = function(t,fb,g,r,b)
+    t:unregister()
+    fb:fill(0,0,0)
+    local ix = 0
+    local c = string.char(g,r,b)
+    fb:set( 2,c) fb:set( 9,c) fb:set(10,c)
+    fb:set(11,c) fb:set(14,c) fb:set(18,c) fb:set(20,c)
+    fb:set(21,c) fb:set(30,c)
+    t:register(500,tmr.ALARM_AUTO,function()
+      if ix == 1
+        then fb:set( 1,c) fb:set( 2,0,0,0) fb:set( 3,c) fb:set( 9,0,0,0) fb:set(11,0,0,0) fb:set(17,c) fb:set(18,0,0,0) fb:set(19,c)
+	else fb:set( 1,0,0,0) fb:set( 2,c) fb:set( 3,0,0,0) fb:ser( 9,c) fb:set(11,c) fb:set(17,0,0,0) fb:set(18,c) fb:set(19,0,0,0)
+      end
+      ix = 1 - ix
+      dodraw()
     end)
   end,
   ["snake"] = function(t,fb,g,r,b)
