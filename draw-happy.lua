4051b50a1e1eabce3b953f262c46b575d6d0aeac
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2016-11-21 16:47:49 -0500

    lamp: draw-happy

---
+++ draw-happy.lua
@@ -0,0 +1,12 @@
+--
+--    1 2 3 4 5 6 7 8
+-- 00 o x o o o o x o
+-- 08 x o x o o x o x
+-- 16 o o o o o o o o
+-- 24 o o o x x o o o
+--
+return function(t,fb,g,r,b)
+  local c = string.char(g,r,b)
+  local i,v
+  for i,v in ipairs({2,7,9,11,14,16,28,29}) do fb:set(v,c) end
+end
