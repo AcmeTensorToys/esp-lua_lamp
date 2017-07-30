13cd7845bb70b332eb0a0930d651b1ad246ff209
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2017-07-29 21:34:55 -0400

    push* scripts: use luasrcdiet if directed
    If ${LUADIET} is empty, send files as they are; otherwise, stream them
    through luasrcdiet.  To use the default options, use 'LUADIET=' '" or
    something to that effect.  This can be set per-file, should some module
    turn out to be broken by dieting.
    
    The "dopush" utility macro is split and now called "dopushlua" and
    "dopushtext" (which skips luasrcdiet even if it is engaged).

--- pushall.sh
+++ pushall.sh
@@ -10,8 +10,8 @@ dopushcompile net/nwfmqtt.lua
 dopushcompile examples/lamp/lamp-touch.lua
 #dopushcompile examples/lamp/lamp-remote.lua
 #dopushcompile examples/lamp/telnetd-cap.lua
-#dopush        examples/lamp/conf/nwfmqtt.conf
-#dopush        examples/lamp/conf/nwfmqtt.subs
+#dopushtext    examples/lamp/conf/nwfmqtt.conf
+#dopushtext    examples/lamp/conf/nwfmqtt.subs
 dopushcompile examples/lamp/init2.lua
 
 #for i in examples/lamp/draw-*.lua; do
