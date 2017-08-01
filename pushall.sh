f635e37b81bfb39f77cb7fea61c95e2a498667bd
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2017-07-31 20:24:26 -0400

    Move to independent repository
    Symlink or checkout core into ./core

--- pushall.sh
+++ pushall.sh
@@ -2,20 +2,20 @@
 
 set -e -u
 
-. ./host/pushcommon.sh
+. ./core/host/pushcommon.sh
 
-dopushcompile net/nwfmqtt.lua
-#dopushcompile cap1188/cap1188.lua
-#dopushcompile cap1188/cap1188-init.lua
-dopushcompile examples/lamp/lamp-touch.lua
-#dopushcompile examples/lamp/lamp-remote.lua
-#dopushcompile examples/lamp/telnetd-cap.lua
-#dopushtext    examples/lamp/conf/nwfmqtt.conf
-#dopushtext    examples/lamp/conf/nwfmqtt.subs
-dopushcompile examples/lamp/init2.lua
+dopushcompile core/net/nwfmqtt.lua
+dopushcompile core/cap1188/cap1188.lua
+dopushcompile core/cap1188/cap1188-init.lua
+dopushcompile lamp-touch.lua
+dopushcompile lamp-remote.lua
+dopushcompile telnetd-cap.lua
+#dopushtext    conf/nwfmqtt.conf
+#dopushtext    conf/nwfmqtt.subs
+dopushcompile init2.lua
 
-#for i in examples/lamp/draw-*.lua; do
-#  dopushcompile ${i}
-#done
+for i in draw-*.lua; do
+  dopushcompile ${i}
+done
 
 echo "SUCCESS"
